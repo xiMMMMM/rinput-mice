ff013db4df085d6db43b686432fdb632a0845814
xiMMMMM <127620979+xiMMMMM@users.noreply.github.com>
2023-07-08 18:05:56 +0200

    Add files via upload

---
+++ Auto RInput to CSGO.bat
@@ -0,0 +1,7 @@
+@ECHO Launching Counter-Strike: Global Offensive/
+@ECHO OFF
+START steam://rungameid/730 
+TIMEOUT /t 10 /nobreak
+START C:\rinputv131seq\RInput csgo.exe 
+TIMEOUT /t 1 /nobreak
+EXIT
